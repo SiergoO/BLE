ba597b38388f79fbf7ed451aafd7b228ef190450
sergey.domashchuk@softeq.com <sergey.domashchuk@softeq.com>
2020-12-15 22:29:42 +0300

    WriteCharacteristicDialog.kt refactored.

--- app/src/main/java/com/example/bleapplication/presentation/dialog/WriteCharacteristicDialog.kt
+++ app/src/main/java/com/example/bleapplication/presentation/dialog/WriteCharacteristicDialog.kt
@@ -15,11 +15,10 @@ import com.example.bleapplication.model.BleCharacteristic
 class WriteCharacteristicDialog(
     private val callback: Callback,
     private var bleCharacteristic: BleCharacteristic
-) :
-    DialogFragment() {
+) : DialogFragment() {
 
-    interface Callback {
-        fun writeButtonClicked(char: BluetoothGattCharacteristic)
+    companion object {
+        var TAG = "Write Characteristic Dialog"
     }
 
     override fun onCreateView(
@@ -29,21 +28,21 @@ class WriteCharacteristicDialog(
     ): View? = inflater.inflate(R.layout.dialog_write_characteristic, container, false)
         .also { dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE) }
 
-override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    val edit = (view.findViewById(R.id.edit_char) as EditText)
-    (view.findViewById(R.id.btn_cancel) as Button).setOnClickListener { dialog?.dismiss() }
-    (view.findViewById(R.id.btn_write) as Button).setOnClickListener {
-        callback.writeButtonClicked(
-            bleCharacteristic.bluetoothGattCharacteristic.apply {
-                value = edit.text.toString().toByteArray()
-            }
-        )
-        dismiss()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val edit = (view.findViewById(R.id.edit_char) as EditText)
+        view.findViewById<Button>(R.id.btn_cancel).setOnClickListener { dialog?.dismiss() }
+        view.findViewById<Button>(R.id.btn_write).setOnClickListener {
+            callback.writeButtonClicked(
+                bleCharacteristic.bluetoothGattCharacteristic.apply {
+                    value = edit.text.toString().toByteArray()
+                }
+            )
+            dismiss()
+        }
+        super.onViewCreated(view, savedInstanceState)
     }
-    super.onViewCreated(view, savedInstanceState)
-}
 
-companion object {
-    var TAG = "Write Characteristic Dialog"
-}
+    interface Callback {
+        fun writeButtonClicked(char: BluetoothGattCharacteristic)
+    }
 }
